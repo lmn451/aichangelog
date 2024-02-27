f925753c0a40a815c75d38d9314860a240650570
lmn451 <putaluta@tuta.io>
2024-02-27 12:01:11 +0200

    refactor: Remove unnecessary code and improve script structure

--- createChangelogs.js
+++ createChangelogs.js
@@ -1,62 +1,91 @@
-const fs = require("fs");
-const path = require("path");
-const OpenAI = require("openai");
+import fs from "fs";
+import OpenAI from "openai";
+import { exec } from "node:child_process";
+import semver from "semver";
+
+const args = process.argv;
+const [, , path = "."] = args;
 const apiKey = process.env.OPENAI_KEY;
 
-const openai = new OpenAI({
-  apiKey,
-});
+const openai = new OpenAI({ apiKey });
+const packageJsonPath = "package.json";
+const changelogFilename = `CHANGELOG_test.md`;
+
+function executeCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      { cwd: path, maxBuffer: 1024 * 1024 * 10 },
+      (error, stdout, stderr) => {
+        if (error) return reject(error);
+        if (stderr) return reject(new Error(stderr));
+        resolve(stdout.trim());
+      }
+    );
+  });
+}
 
 async function sendToOpenAI(diff) {
-  try {
-    const messages = [
-      {
-        role: "system",
-        content:
-          "You are principal dev who loves and knows how to write concise and descriptive changelogs",
-      },
-      {
-        role: "user",
-        content: `Extract a changelog from the following diff:\n${diff}`,
-      },
-    ];
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo", // adjust for specific model
-      messages: messages,
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a principal dev who loves and knows how to write concise and descriptive changelogs informative for business.",
+    },
+    {
+      role: "user",
+      content: `Extract a concise and informative changelog from the following diff:\n${diff}
+        ### Version {{ version }}
+#### Changes in {{filename}}
+- {{changes}}
+- {{changes}}
+
+#### Changes in {{anotherfile}}
+- {{changes}}
+- {{changes}}
+
+TLDR: {{ concise description of changes}}`,
+    },
+  ];
+
+  return openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages,
+    })
+    .then((response) => response.choices[0].message.content)
+    .catch((error) => {
+      console.error("Error sending data to OpenAI:", error);
+      throw error;
     });
-    return response.choices[0].message.content; // extract changelog
-  } catch (error) {
-    console.error("Error sending data to OpenAI:", error);
-    // Handle API-specific errors here
-  }
 }
+const commitsString = await executeCommand(
+  `git log -p -G'"version": "[0-9]+\\.[0-9]+\\.[0-9]+"' --pretty=format:"%H" -- ${packageJsonPath} | grep -oE '^[a-f0-9]{40}$'`
+);
+const hashes = commitsString.split("\n").filter(Boolean).reverse();
+const changelogEntries = await Promise.all(
+  hashes.slice(0, -1).map(async (currentCommit, i) => {
+    const nextCommit = hashes[i + 1];
+    const packageJsonString = await executeCommand(
+      `git show ${nextCommit}:${packageJsonPath}`
+    );
+    const packageJson = JSON.parse(packageJsonString);
+    const diff = await executeCommand(
+      `git diff ${currentCommit} ${nextCommit} -- . ':(exclude)*lock*' ':(exclude)*changelog*'`
+    );
+    const changelogEntry = await sendToOpenAI(diff);
+    return { version: packageJson.version, content: changelogEntry };
+  })
+);
 
-// Function to get a list of all diff files
-function getDiffFiles(dirPath) {
-  return fs
-    .readdirSync(dirPath)
-    .filter((file) => file.startsWith("diff_"))
-    .map((file) => path.join(dirPath, file));
-}
+const sortedChangelogEntries = changelogEntries.sort((a, b) =>
+  semver.compare(b.version, a.version)
+);
+const changelogContent = sortedChangelogEntries
+  .map((entry) => entry.content)
+  .join("\n\n");
 
-// Function to read file content
-function getFileContent(filePath) {
-  return fs.readFileSync(filePath, { encoding: "utf8" });
-}
-
-// The main function to generate changelog
-async function generateChangelog() {
-  const diffFiles = getDiffFiles("."); // Adjust the path if necessary
-
-  for (const file of diffFiles) {
-    const diff = getFileContent(file);
-    const filename = file.substring(file.lastIndexOf("diff"));
-    const changelog = await sendToOpenAI(diff);
-    const changelogFilename = `changelog_${filename.replace("diff_", "")}`;
-
-    // Write the changelog to a file
-    fs.writeFileSync(changelogFilename, changelog);
-  }
-}
-
-generateChangelog();
+fs.writeFileSync(
+  changelogFilename,
+  `# Changelog  [created by aichangelog](https://github.com/lmn451/aichangelog) \n\n${changelogContent}`
+);
